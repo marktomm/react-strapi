e6cd87f5e64ad3655dd1b336d879d28353e688d8
Liona <liona.tomm@nextguest.com>
2023-04-06 16:39:26 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// App.js
+
+import React from 'react';
+// import './App.css';
+import Component from './Component';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Example App</h1>
+      </header>
+      <main>
+        <Component />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component.js
@@ -0,0 +1,46 @@
+// Component.js
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Component = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.example.com/data');
+        setData(response.data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Data:</h2>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Component;
